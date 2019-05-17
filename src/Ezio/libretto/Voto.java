d97e30463839b20377cfaa0344b3c2099ce94504
Ezio <Ezio@DESKTOP-7IIA0AT.station>
2019-05-17 16:14:38 +0200

    Aggiunti metodi standard

--- src/Ezio/libretto/Voto.java
+++ src/Ezio/libretto/Voto.java
@@ -1,5 +1,11 @@
 package Ezio.libretto;
 
-public class Voto {
+import java.time.LocalDate;
 
+public class Voto {
+   private int voto;
+   private String corso;
+   private LocalDate date;    //oggetto per la gestione delle date
+   
+   
 }
