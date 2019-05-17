0b659b84edcdb84e6550ded59dde996f970e21ad
Ezio <Ezio@DESKTOP-7IIA0AT.station>
2019-05-17 16:26:17 +0200

    Aggiunti metodi get e setter

--- src/Ezio/libretto/Voto.java
+++ src/Ezio/libretto/Voto.java
@@ -6,6 +6,24 @@ public class Voto {
    private int voto;
    private String corso;
    private LocalDate date;    //oggetto per la gestione delle date
+public int getVoto() {
+	return voto;
+}
+public void setVoto(int voto) {
+	this.voto = voto;
+}
+public String getCorso() {
+	return corso;
+}
+public void setCorso(String corso) {
+	this.corso = corso;
+}
+public LocalDate getDate() {
+	return date;
+}
+public void setDate(LocalDate date) {
+	this.date = date;
+}
    
    
 }
